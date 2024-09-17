494c53d55d98da625e8db7687aed605a96bec2c9
Ron Shuvy <ronsh@wix.com>
2024-09-17 17:53:44 +0300

    refactor

--- src/backend/service-plugins/ecom-validations/purchase-limit/plugin.ts
+++ src/backend/service-plugins/ecom-validations/purchase-limit/plugin.ts
@@ -6,27 +6,28 @@ import type {PurchaseRules} from "../../../../types";
 validations.provideHandlers({
     getValidationViolations: async ({request, metadata}): Promise<validations.GetValidationViolationsResponse> => {
         const rules = await getPurchaseRules();
-        const minSubtotal = rules.minSubtotal || 0;
-        const maxSubtotal = rules.maxSubtotal || Number.MAX_SAFE_INTEGER;
-        // @ts-ignore
-        const subtotal = parseInt(request.validationInfo.priceSummary.subtotal.amount);
+        const subtotal = parseInt(request?.validationInfo?.priceSummary?.subtotal?.amount || "0");
+        const totalItems = request?.validationInfo?.lineItems?.length || 0
         const validations = [
             {
-                predicate: () => {
-                    return subtotal >= minSubtotal
-                },
-                errorMessage: `Your total purchase amount cannot be lower then ${minSubtotal}`
+                predicate: () => { return subtotal >= rules.minSubtotal },
+                errorMessage: `Your total purchase amount cannot be lower then ${rules.minSubtotal}`
             },
             {
-                predicate: () => {
-                    return subtotal <= maxSubtotal
-                },
-                errorMessage: `Your total purchase amount cannot be higher then ${maxSubtotal}`
+                predicate: () => { return subtotal <= rules.maxSubtotal },
+                errorMessage: `Your total purchase amount cannot be higher then ${rules.maxSubtotal}`
+            },
+            {
+                predicate: () => { return totalItems >= rules.minTotalItems },
+                errorMessage: `The total number of items cannot be lower then ${rules.minTotalItems}`
+            },
+            {
+                predicate: () => { return totalItems <= rules.maxTotalItems },
+                errorMessage: `The total number of items cannot be higher then ${rules.maxTotalItems}`
             },
         ];
-
         const violations = validations
-            .filter(({ predicate }) => !predicate()) // Ensure predicate is called as a function
+            .filter(({ predicate }) => !predicate())
             .map(({ errorMessage }) => createViolation(errorMessage));
 
         return {violations: violations}
@@ -35,7 +36,14 @@ validations.provideHandlers({
 
 async function getPurchaseRules() {
     const collection = await getDataFromCollection({dataCollectionId: PURCHASE_RULES_COLLECTION_ID});
-    return collection.items[0]?.data as PurchaseRules;
+    const data = collection.items[0]?.data as PurchaseRules;
+
+    return {
+        minSubtotal: data.minSubtotal || 0,
+        maxSubtotal: data.maxSubtotal || Number.MAX_SAFE_INTEGER,
+        minTotalItems: data.minTotalItems || 0,
+        maxTotalItems: data.maxTotalItems || Number.MAX_SAFE_INTEGER,
+    };
 }
 
 function createViolation(description: string): validations.Violation {
