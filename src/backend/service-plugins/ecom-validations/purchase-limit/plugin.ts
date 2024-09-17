ed9f00c3a4f84da9824b0e17c009c9d1d4e91648
Ron Shuvy <ronsh@wix.com>
2024-09-17 15:41:20 +0300

    implement SPI

--- src/backend/service-plugins/ecom-validations/purchase-limit/plugin.ts
+++ src/backend/service-plugins/ecom-validations/purchase-limit/plugin.ts
@@ -1,21 +1,36 @@
 import {validations} from '@wix/ecom/service-plugins/context';
+import {getDataFromCollection} from "../../../database";
+import {PURCHASE_RULES_COLLECTION_ID} from "../../../consts";
+import type {Settings} from "../../../../types";
 
 validations.provideHandlers({
     getValidationViolations: async ({request, metadata}): Promise<validations.GetValidationViolationsResponse> => {
-        const subtotal = 50 // parseInt(request.validationInfo.priceSummary.total.amount)
-        let violations = [];
-        let severity = validations.Severity.ERROR
-        let target: validations.Target = {
-            other: {
-                name: validations.NameInOther.OTHER_DEFAULT
+        // @ts-ignore
+        const subtotal = parseInt(request.validationInfo.priceSummary.subtotal.amount);
+        const minSubtotal = await getMinSubtotal();
+
+        if (subtotal < minSubtotal) {
+            let description = "You can't purchase a total amount that is lower than 100"
+            let severity = validations.Severity.ERROR
+            let target: validations.Target = {
+                other: {
+                    name: validations.NameInOther.OTHER_DEFAULT
+                }
             }
+            let violation = createViolation(severity, target, description);
+
+            return {violations: [violation]}
+        } else {
+            return {violations: []}
         }
-        let description = "You can't purchase a total amount that is lower than 100"
-        // const violation = createViolation(severity, target, description);
-        // violations.push(violation);
-        return {violations: [createViolation(severity, target, description)]}
     }
 })
+
+async function getMinSubtotal() {
+    const collection = await getDataFromCollection({dataCollectionId: PURCHASE_RULES_COLLECTION_ID});
+    const collectionData = collection.items[0]?.data as Settings;
+    return collectionData?.minSubtotal;
+}
 
 function createViolation(severity: validations.Severity, target: validations.Target, description: string): validations.Violation {
     return {severity, target, description};
