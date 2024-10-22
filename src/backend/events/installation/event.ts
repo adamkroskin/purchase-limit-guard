ba0b1edaa36af89465b08a7d2ec248a37dc5ae13
yaird <yaird@wix.com>
2024-10-22 17:32:25 +0300

    wrap installtion event with try catch

--- src/backend/events/installation/event.ts
+++ src/backend/events/installation/event.ts
@@ -1,24 +1,28 @@
-import { auth } from '@wix/essentials';
-import { collections } from '@wix/data';
-import { appInstances } from '@wix/app-management';
-import { PURCHASE_RULES_COLLECTION_ID, PURCHASE_RULES_COLLECTION_NAME } from '../../consts';
+import {auth} from '@wix/essentials';
+import {collections} from '@wix/data';
+import {appInstances} from '@wix/app-management';
+import {PURCHASE_RULES_COLLECTION_ID, PURCHASE_RULES_COLLECTION_NAME} from '../../consts';
 import type {PurchaseRules} from "../../../types";
 
 appInstances.onAppInstanceInstalled(() => {
-  auth.elevate(collections.createDataCollection)({
-    _id: PURCHASE_RULES_COLLECTION_ID,
-    displayName: PURCHASE_RULES_COLLECTION_NAME,
-    fields: [
-      { key: 'subtotal', type: collections.Type.OBJECT },
-      { key: 'totalItems', type: collections.Type.OBJECT },
-      { key: 'orderWeight', type: collections.Type.NUMBER },
-    ],
-    permissions: {
-      // Make sure to change the permissions according to the actual usage of your collection
-      insert: collections.Role.ANYONE,
-      read: collections.Role.ANYONE,
-      remove: collections.Role.ANYONE,
-      update: collections.Role.ANYONE,
-    },
-  });
+    try {
+        auth.elevate(collections.createDataCollection)({
+            _id: PURCHASE_RULES_COLLECTION_ID,
+            displayName: PURCHASE_RULES_COLLECTION_NAME,
+            fields: [
+                {key: 'subtotal', type: collections.Type.OBJECT},
+                {key: 'totalItems', type: collections.Type.OBJECT},
+                {key: 'orderWeight', type: collections.Type.NUMBER},
+            ],
+            permissions: {
+                // Make sure to change the permissions according to the actual usage of your collection
+                insert: collections.Role.ANYONE,
+                read: collections.Role.ANYONE,
+                remove: collections.Role.ANYONE,
+                update: collections.Role.ANYONE,
+            },
+        });
+    } catch (error) {
+        console.log("App Installation Error: ", error);
+    }
 });
